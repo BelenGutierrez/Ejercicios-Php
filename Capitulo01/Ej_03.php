92f73339f7061f5f96c7aee9aa4cecc1ee8ea19b
BelenGutierrez <belencitagutierrez@hotmail.com>
2016-09-23 10:58:28 +0200

    Subiendo Ejercicios Tema 1

---
+++ Capitulo01/Ej_03.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        echo "<table>";
+        ?>
+        <table>
+            <tr><td>Dog</td><td>Perro</td></tr>
+            <tr><td>Yellow</td><td>Amarillo</td></tr>
+            <tr><td>Blue</td><td>Azul</td></tr>
+            <tr><td>Storm</td><td>Tormenta</td></tr>
+            <tr><td>Flat</td><td>Apartamento</td></tr>
+            <tr><td>Meal</td><td>Comida</td></tr>
+            <tr><td>Sugar</td><td>Azucar</td></tr>
+            <tr><td>Past</td><td>Pasado</td></tr>
+            <tr><td>Fire</td><td>Fuego</td></tr>
+            <tr><td>Room</td><td>Cuarto</td></tr>
+        </table>
+        <?php
+        echo "</table>";
+        ?>
+    </body>
+</html>
---
+++ Capitulo01/Ej_09.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        $e = 166.386;
+        $p = 100;
+        $resultado = $p / $e ;
+
+        echo " $p  pesetas son:  $resultado  euros.";
+        ?>
+    </body>
+</html>
